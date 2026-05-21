7ccf0faee63479c3620ad6bb62c3bc0294783e8a
bildan <bildanjauhary@gmail.com>
2026-05-21 10:44:33 +0700

    feat: add function to convert fahreneheit to rankine

---
+++ utils/fahrenheit.js
@@ -0,0 +1,12 @@
+// Implement functions here 👇
+
+// TODO: Uncomment after implemented
+module.exports = {
+  // fahrenheitToCelsius,
+  // fahrenheitToKelvin,
+  fahrenheitToRankine,
+};
+
+function fahrenheitToRankine(value){
+  return (value + 459.67)
+}
