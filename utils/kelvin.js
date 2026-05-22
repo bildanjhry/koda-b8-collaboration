f85f18e86572c90178bfa5dadde2127e1259fd6e
bildan <bildanjauhary@gmail.com>
2026-05-22 14:48:51 +0700

    refactor: utilizing validation input to all function

--- utils/kelvin.js
+++ utils/kelvin.js
@@ -1,10 +1,19 @@
+function validateNumber(value) {
+  if (typeof value !== "number" || isNaN(value)) {
+    throw new Error("Input harus berupa angka valid");
+  }
+}
+
 function kelvinToCelsius(value) {
+  validateNumber(value);
   return value - 273.15;
 }
 function kelvinToRankine(value) {
+  validateNumber(value);
   return (value * 9) / 5;
 }
 function kelvinToFahrenheit(value) {
+  validateNumber(value);
   return ((value - 273.15) * 9) / 5 + 32;
 }
 
